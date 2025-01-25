dd9a80861aa32c460bffea77137ecfc308e60a78
W0NYV <w0nyv.707@gmail.com>
2025-01-25 14:01:31 +0900

    first commit

---
+++ sample.hlsl
@@ -0,0 +1,19 @@
+
+cbuffer cb : register(b0)
+{
+    float2 resolution;
+    float time;
+    float globalBPM;
+
+    float param0;
+    float param1;
+    float param2;
+    float param3;
+};
+
+
+float4 Frag(VsOutput input) : SV_TARGET
+{
+    float2 uv = (input.uv * 2 - 1) * 0.5;
+    return float4(uv.x,uv.y,0.0, 1.0);
+}
